7b09c148f5a7e133ae1b8ac2fbee6048ae53bffe
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-14 19:34:25 -0500

    created an array of skills objects to be fetched for the skills component

---
+++ src/app/components/skillsList.js
@@ -0,0 +1,70 @@
+export const skills = [
+  {
+    name: "HTML",
+    src: "html",
+  },
+  {
+    name: "CSS",
+    src: "css",
+  },
+  {
+    name: "Bootstrap",
+    src: "bootstrap",
+  },
+  {
+    name: "Tailwind CSS",
+    src: "tailwind",
+  },
+  {
+    name: "Javascript",
+    src: "javascript",
+  },
+  {
+    name: "Postman",
+    src: "postman",
+  },
+  {
+    name: "Git",
+    src: "git",
+  },
+  {
+    name: "Rest API",
+    src: "api",
+  },
+  {
+    name: "SASS",
+    src: "sass",
+  },
+  {
+    name: "Node Js",
+    src: "node",
+  },
+  {
+    name: "Next Js",
+    src: "next",
+  },
+  {
+    name: "React",
+    src: "react",
+  },
+  {
+    name: "Framer Motion",
+    src: "framer",
+  },
+  {
+    name: "Jest",
+    src: "jest",
+  },
+  {
+    name: "Webpack",
+    src: "webpack",
+  },
+  {
+    name: "Babel",
+    src: "babel",
+  },
+  {
+    name: "Firebase",
+    src: "firebase",
+  },
+];
