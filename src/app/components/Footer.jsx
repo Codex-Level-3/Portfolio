dc25b41d6745688b57430590a06092835d64c435
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-14 19:24:34 -0500

    Added and styled a footer component with tailwind

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center h-16 w-full font-semibold text-lg">
+      <p>&copy; Created by Laqwanda Nettles</p>
+    </footer>
+  );
+}
