3959e53e9ce465e177ac18ed1eb1fa7389479580
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-15 15:16:55 -0500

    implemented embla's carousel

--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -1,30 +1,61 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import SkillCard from "./SkillCard";
-import Carousel from "react-bootstrap/Carousel";
 import { skills } from "./skillsList";
 import Image from "next/image";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function Skills() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+
+  useEffect(() => {
+    let timerId;
+    if (emblaApi) {
+      // Start autoplay manually
+      const startAutoplay = () => {
+        timerId = setInterval(() => {
+          emblaApi.scrollNext();
+        }, 2000);
+      };
+
+      // Call startAutoplay to initiate autoplay
+      startAutoplay();
+
+      // Stop autoplay when the component unmounts
+      return () => {
+        clearInterval(timerId);
+      };
+    }
+  }, [emblaApi]);
+
   return (
-    <section>
-      <h2>Skills</h2>
-      <div>
-        {skills.map((skill, index) => (
-          <Carousel key={index}>
-            <Carousel.Item interval={1000}>
-              <Image
-                width={150}
-                height={150}
-                src={`/skills/${skill.src}.png`}
-                alt={skill.name + " Logo"}
-              />
-              <Carousel.Caption>
-                <h3>{skill.name}</h3>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        ))}
+    <section className="bg-gradient-to-r from-zinc-900 to-[#3b2869] mt-4 pt-2 pb-8">
+      <h2 className="text-center font-bold text-[#f8fdea] text-3xl p-2">
+        Skills
+      </h2>
+
+      <div className="embla overflow-hidden" ref={emblaRef}>
+        <div className="embla__container flex">
+          {skills.map((skill, index) => (
+            <div key={index} className="embla__slide flex-[0_0_35%]">
+              <div className="backdrop-blur-sm bg-black/30 shadow-xl shadow-cyan-500/30 h-[280px] w-[200px] pt-4 rounded-lg overflow-hidden mb-3 mt-3 mx-auto font-[sans-serif] flex flex-col justify-center items-center">
+                <Image
+                  className="w-full"
+                  width={150}
+                  height={150}
+                  src={`/skills/${skill.src}.png`}
+                  alt={skill.name + " Logo"}
+                />
+                <div className="px-4 py-6">
+                  <h3 className="text-[#f8fdea] text-xl font-bold text-center">
+                    {skill.name}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
