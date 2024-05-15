7a90d253fae34ca18879d0d0246730a6238c8ad8
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-14 19:34:53 -0500

    component to display projects

---
+++ src/app/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+export default function Projects() {
+  return (
+    <div>
+      <ProjectCard
+        projectHeader="1"
+        title="E-commerce Website"
+        description="This a description for project 1"
+      />
+    </div>
+  );
+}
