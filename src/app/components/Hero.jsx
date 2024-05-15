843bd3501460c7d75943995e9313105fea235545
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-15 15:16:26 -0500

    added link to contact section

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
       </h2>
       <div className="flex justify-normal items-center gap-6 pt-4 text-center">
         <div className="cursor-pointer px-6 py-3 min-w-[140px] shadow-lg shadow-purple-200 rounded-full text-[#fee9ce] text-lg tracking-wider font-medium outline-none border-2 border-purple-600 active:shadow-inner hover:bg-purple-600 hover:text-[#f8fdea] transition-all duration-300">
-          <Link href="#">Contact Me</Link>
+          <Link href="#contact">Contact Me</Link>
         </div>
         <div className="cursor-pointer">
           <Link
