bb617105bf25670feef3247fde51f4af839c0464
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-15 20:02:12 -0500

    applied tailwind styling

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function ProjectCard({ projectHeader, title, description }) {
   return (
     <div>
-      <h2>{`Project ${projectHeader}`}</h2>
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <h2 className="text-2xl font-semibold text-[#9997bc]">{`Project ${projectHeader}`}</h2>
+      <h3 className="text-xl text-[#fee9ce]">{title}</h3>
+      <p className="text-xl text-[#f8fdea]">{description}</p>
     </div>
   );
 }
