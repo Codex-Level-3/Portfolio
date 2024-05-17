2953394c3fb75050bd7d2943e7cd77c4541d0ec6
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-16 20:46:15 -0500

    Create a component for ecommerce project

--- src/app/projects/components/Ecommerce.jsx
+++ src/app/projects/components/Ecommerce.jsx
@@ -1,24 +1,33 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import GitHubSvg from "./GitHubSvg";
 
 export default function Ecommerce() {
   return (
     <section id="ecommerce">
+      <h2>Project 1</h2>
       <Image
-        href="./ecommerce.png"
+        src="/ecommerce.png"
         alt="ecommerce site"
         height={300}
         width={300}
       />
-      <h2>Pitmaster Back Alley BBQ</h2>
+      <h3>Pitmaster Back Alley BBQ</h3>
       <p>
-        This is one of my first projects that I built. It is a fake E-commerce
-        website that is based off the the real restaurant. It implements basic
-        HTML, CSS, and Bootstrap. This project was my introduction into coding
-        for the very first time - Discovering HTML elements and attributes,
-        styling css selectors, and adding bootstrap components to enhance the
-        functionality and appeal to the site.
+        This is one of the first projects I built: a fake E-commerce website
+        inspired by a real restaurant. It incorporates basic HTML, CSS, and
+        Bootstrap. This project marked my initial exploration into coding, where
+        I discovered HTML elements and attributes, styling CSS selectors, and
+        integrated Bootstrap components to enhance the functionality and appeal
+        of the site.
       </p>
+      <Link href="https://github.com/Laqwanda-Nettles/Bootstrap-Project">
+        <GitHubSvg />
+      </Link>
+      <Link href="https://laqwanda-nettles.github.io/Bootstrap-Project/">
+        Visit Site
+      </Link>
     </section>
   );
 }
