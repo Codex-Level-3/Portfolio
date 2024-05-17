46d23f91572ea661eab298fb4fb23386fd803093
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-16 20:45:12 -0500

    Imported components

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Ecommerce from "./components/Ecommerce";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Breadcrumb from "./components/Breadcrumb";
 
 export default function page() {
   return (
     <div>
+      <Header />
       <h1>Project Overviews:</h1>
+      <Breadcrumb />
       <Ecommerce />
+      <Footer />
     </div>
   );
 }
