c954a09f61428734514e0d8a17c908e28d2bdd2e
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-19 11:55:56 -0500

    applied tailwind css styles

--- src/app/projects/components/Ecommerce.jsx
+++ src/app/projects/components/Ecommerce.jsx
@@ -5,16 +5,21 @@ import GitHubSvg from "./GitHubSvg";
 
 export default function Ecommerce() {
   return (
-    <section id="ecommerce">
-      <h2>Project 1</h2>
+    <section
+      className="m-4 border-b-4 border-indigo-500 flex flex-col justify-center items-center"
+      id="ecommerce"
+    >
+      <h2 className="text-3xl font-semibold text-[#f8fdea] mb-4">Project 1</h2>
       <Image
         src="/ecommerce.png"
         alt="ecommerce site"
         height={300}
         width={300}
       />
-      <h3>Pitmaster Back Alley BBQ</h3>
-      <p>
+      <h3 className="text-2xl text-[#fee9ce] font-semibold m-3">
+        Pitmaster Back Alley BBQ
+      </h3>
+      <p className="text-xl text-slate-100">
         This is one of the first projects I built: a fake E-commerce website
         inspired by a real restaurant. It incorporates basic HTML, CSS, and
         Bootstrap. This project marked my initial exploration into coding, where
@@ -22,12 +27,17 @@ export default function Ecommerce() {
         integrated Bootstrap components to enhance the functionality and appeal
         of the site.
       </p>
-      <Link href="https://github.com/Laqwanda-Nettles/Bootstrap-Project">
-        <GitHubSvg />
-      </Link>
-      <Link href="https://laqwanda-nettles.github.io/Bootstrap-Project/">
-        Visit Site
-      </Link>
+      <div className="flex jusify-center items-center gap-5 mb-3 mt-3">
+        <Link href="https://github.com/Laqwanda-Nettles/Bootstrap-Project">
+          <GitHubSvg />
+        </Link>
+        <Link
+          className="text-lg text-white font-semibold hover:text-amber-200"
+          href="https://laqwanda-nettles.github.io/Bootstrap-Project/"
+        >
+          Visit Site
+        </Link>
+      </div>
     </section>
   );
 }
