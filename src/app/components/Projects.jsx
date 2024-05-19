e4bb22c38fca12a2a43a7af44e8a1d2e5ff9f348
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-19 11:54:21 -0500

    applied framer motion to images

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,7 +1,9 @@
+"use client";
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   return (
@@ -18,17 +20,29 @@ export default function Projects() {
           title="E-commerce Website"
           description="Pitmasters BBQ was one of my first projects it features my knowledge of HTML, CSS, and Bootstrap."
         />
-        <Link href="/projects">
-          <Image
-            src="/ecommerce.png"
-            alt="ecommerce website"
-            height={200}
-            width={200}
-          />
-        </Link>
+        <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <Link href="/projects#ecommerce">
+            <Image
+              src="/ecommerce.png"
+              alt="ecommerce website"
+              height={200}
+              width={200}
+            />
+          </Link>
+        </motion.button>
       </div>
       <div className="flex gap-6 items-center">
-        <Image src="/blog.png" alt="blog website" height={200} width={200} />
+        <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <Link href="/projects#blog">
+            <Image
+              src="/blog.png"
+              alt="blog website"
+              height={200}
+              width={200}
+            />
+          </Link>
+        </motion.button>
+
         <ProjectCard
           projectHeader="2"
           title="Blog"
@@ -41,20 +55,29 @@ export default function Projects() {
           title="Recipe App"
           description="What's for Dinner? project incorporated API integration, object-oriented programming, CSS Animations, and SCSS 7-1 architecture."
         />
-        <Image
-          src="/recipe.png"
-          alt="recipe website"
-          height={200}
-          width={200}
-        />
+        <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <Link href="/projects#recipes">
+            <Image
+              src="/recipe.png"
+              alt="recipe website"
+              height={250}
+              width={250}
+            />
+          </Link>
+        </motion.button>
       </div>
       <div className="flex gap-6 items-center">
-        <Image
-          src="/community.png"
-          alt="community website"
-          height={200}
-          width={200}
-        />
+        <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <Link href="/projects#community">
+            <Image
+              src="/community.png"
+              alt="community website"
+              height={250}
+              width={250}
+            />
+          </Link>
+        </motion.button>
+
         <ProjectCard
           projectHeader="4"
           title="Community Website"
