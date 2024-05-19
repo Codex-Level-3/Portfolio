60500251fe6d2c59ea1004d893903026b6f9fbef
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-19 11:56:56 -0500

    applied tailwind and imported blog, recipe, and community components

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,14 +3,20 @@ import Ecommerce from "./components/Ecommerce";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Breadcrumb from "./components/Breadcrumb";
+import Blog from "./components/Blog";
+import Recipe from "./components/Recipe";
+import Community from "./components/Community";
 
 export default function page() {
   return (
     <div>
       <Header />
-      <h1>Project Overviews:</h1>
+      <h1 className="text-4xl text-white font-bold m-4">Project Overviews:</h1>
       <Breadcrumb />
       <Ecommerce />
+      <Blog />
+      <Recipe />
+      <Community />
       <Footer />
     </div>
   );
