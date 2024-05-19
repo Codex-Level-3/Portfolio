432779f4408b7ed4e148eabdca665145a6c58ea3
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-19 11:51:34 -0500

    add about me paragraph

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -11,10 +11,16 @@ export default function About() {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold text-[#f8fdea]">About Me</h1>
         <p className="text-xl text-[#fee9ce]">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+          Hi there! I&apos;m Laqwanda, a passionate software developer with a
+          drive to learn and delve deeper into the world of technology and
+          innovation. Currently, I'm pursuing a Full-Stack Software Development
+          certificate from CodeX Academy. With a focus on building a strong
+          foundation in coding, a knack for problem-solving, a keen eye for
+          detail, and a commitment to continuous learning, I aim to deliver
+          robust and user-centric software solutions. From front-end development
+          to back-end solutions, I'm constantly refining my skills to create
+          seamless experiences for users. Let's connect and explore the endless
+          possibilities of technology together!
         </p>
       </div>
     </section>
