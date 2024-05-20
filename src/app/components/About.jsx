277dbcf88996fcb28a8c890b1561af5b6c64bb6c
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-05-20 01:15:01 -0500

    added image and edited about me paragraph

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -7,19 +7,29 @@ export default function About() {
       id="about"
       className="flex flex-wrap gap-5 justify-around items-center m-6"
     >
-      <Image src="/placeholder.png" alt="pic of me" width={300} height={300} />
+      <Image
+        className="object-top h-52 object-cover rounded-full transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 bg-indigo-500 shadow-lg shadow-indigo-500/50 hover:shadow-2xl hover:shadow-indigo-500/50"
+        src="/me1.png"
+        alt="pic of me"
+        width={300}
+        height={300}
+      />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold text-[#f8fdea]">About Me</h1>
         <p className="text-xl text-[#fee9ce]">
           Hi there! I&apos;m Laqwanda, a passionate software developer with a
-          drive to learn and delve deeper into the world of technology and
-          innovation. Currently, I&apos;m pursuing a Full-Stack Software
-          Development certificate from CodeX Academy. With a focus on building a
-          strong foundation in coding, a knack for problem-solving, a keen eye
-          for detail, and a commitment to continuous learning, I aim to deliver
-          robust and user-centric software solutions. From front-end development
-          to back-end solutions, I&apos;m constantly refining my skills to
-          create seamless experiences for users. Let&apos;s connect and explore
+          unique background in Culinary Arts and a drive to delve deeper into
+          technology and innovation. Currently, I&apos;m pursuing a Full-Stack
+          Software Development certificate from CodeX Academy. My journey from
+          the kitchen to coding has honed my creativity and precision, allowing
+          me to tackle problem-solving with a versatile and adaptable approach.
+        </p>
+        <p className="text-xl text-[#fee9ce]">
+          I focus on building a strong foundation in coding, with a keen eye for
+          detail and a commitment to continuous learning. From front-end
+          development to back-end solutions, I strive to create seamless,
+          user-centric experiences. My eagerness to learn drives me to explore
+          new languages and innovative projects. Let&apos;s connect and explore
           the endless possibilities of technology together!
         </p>
       </div>
